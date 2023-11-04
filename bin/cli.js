be74a3ffc5f65d7cb4da28e247e7a8e3687c185c
aryankarim <aryanmajid97@gmail.com>
2023-11-04 16:37:17 +0300

    finalize npm scripts

---
+++ bin/cli.js
@@ -0,0 +1,27 @@
+#! usr/bin/env node
+
+const { execSync } = require("child_process");
+
+const runCommand = () => {
+  try {
+    execSync(`${command}`, { stdio: "inherit" });
+  } catch (error) {
+    console.log(`Failed to execute ${command}`, error);
+    return false;
+  }
+  return true;
+};
+
+const repoName = process.argv[2];
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/aryankarim/nuxt-threejs-tailwind ${repoName}`;
+const installDepsCommand = `cd ${repoName} && yarn install`;
+
+console.log(`Cloning the repository '${repoName}'`);
+const checkedOut = runCommand(gitCheckoutCommand);
+if (!checkedOut) process.exit(-1);
+
+console.log(`Cloning the repository '${repoName}'`);
+const installDeps = runCommand(installDepsCommand);
+if (!installDeps) process.exit(-1);
+
+console.log("Nice Work! Now impress me!");
